3652330559f832cef324a0219aa4589f4a878518
Kushal Bhana <kushalbhana2050@gmail.com>
2025-09-10 01:39:39 +0530

    Added hero circuit animation

---
+++ apps/web/components/home/hero-circuit.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import Rive from "@rive-app/react-canvas";
+
+export const HeroCircuit = () => (
+    <div className="w-screen h-screen">
+      <Rive
+        src="animations/Hero-Circuit-animation.riv"
+
+      />
+
+    </div>
+);
